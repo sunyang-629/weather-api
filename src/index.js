3ffdc475e855e9db999d1becfa22599ce198e66f
sunyang9876 <sunyang9876@hotmail.com>
2019-11-13 16:13:16 +1000

    env var

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const routes = require("./routes");
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log("server has started at port 3000");
+app.listen(PORT, () => {
+  console.log(`server has started at port ${PORT}`);
 });
--- src/routes/weather.js
+++ src/routes/weather.js
@@ -3,11 +3,13 @@ const axios = require("axios");
 
 const router = express.Router();
 
+const APPID = process.env.APPID;
+
 router.get("/", (req, res) => {
   const { cc, city } = req.query;
   axios
     .get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city},${cc}&appid=4c4a824387562138724ba3f653cb1424`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${cc}&appid=${APPID}`
     )
     .then(response => {
       res.send(response.data);
