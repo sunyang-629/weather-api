dfa288a81ff541bf7ab8e423783447536962e1ac
sunyang9876 <sunyang9876@hotmail.com>
2019-11-13 17:48:15 +1000

    add morgan winston helmet

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 const routes = require("./routes");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const logger = require("./utils/logger");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(helmet());
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+} else {
+  app.use(morgan("common"));
+}
 app.use(routes);
 
 app.listen(PORT, () => {
-  console.log(`server has started at port ${PORT}`);
+  logger.info(`server has started at port ${PORT}`);
 });
