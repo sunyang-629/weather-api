61077f51d344ac6243018137a58de913539e4c5e
sunyang9876 <sunyang9876@hotmail.com>
2019-11-14 18:32:08 +1000

    add country name validator

---
+++ src/middlewares/counrtryValidator.js
@@ -0,0 +1,17 @@
+const { getCode, getName } = require("country-list");
+const responseFormatter = require("./../utils/responseFormatter");
+
+module.exports = (req, res, next) => {
+  const { cc } = req.params;
+  if (cc.length === 2) {
+    if (getName(cc)) {
+      return next();
+    }
+  } else {
+    if (getCode(cc)) {
+      req.params.cc = getName(cc);
+      return next();
+    }
+  }
+  return responseFormatter(res, 400, "Invalid country name or country code");
+};
--- src/routes/weather.js
+++ src/routes/weather.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const responseFormatter = require("./../utils/responseFormatter");
 const weather = require("./../models/Weather");
+const countryValidator = require("./../middlewares/counrtryValidator");
 
 const router = express.Router();
 
 // const APPID = process.env.APPID;
 
-router.get("/:cc/:city", (req, res) => {
+router.get("/:cc/:city", countryValidator, (req, res, next) => {
   const { cc, city } = req.params;
   const weatherType = req.query.weatherType;
   weather
     .getDate(city, cc, weatherType)
     .then(response => responseFormatter(res, 200, null, response))
-    .catch(err => console.log(err));
+    .catch(err => next(err));
 });
 
 module.exports = router;
